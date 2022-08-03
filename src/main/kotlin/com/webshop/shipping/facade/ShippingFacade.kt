d09ad1dca60d31b1761062a0fc27208bd6d5fded
Szabolcs Torma <szabolcs.torma@accretivetg.com>
2022-08-03 18:53:34 +0200

    shipping dto transformer

--- src/main/kotlin/com/webshop/shipping/facade/ShippingFacade.kt
+++ src/main/kotlin/com/webshop/shipping/facade/ShippingFacade.kt
@@ -1,6 +1,7 @@
 package com.webshop.shipping.facade
 
 import com.webshop.shipping.entity.Shipping
+import com.webshop.shipping.model.ShippingDto
 import com.webshop.shipping.service.ShippingService
 import org.springframework.stereotype.Component
 
@@ -9,5 +10,15 @@ class ShippingFacade(val shippingService: ShippingService) {
 
     fun getAllShipping(): MutableIterable<Shipping> {
         return shippingService.getAllShipping()
+    }
+
+    fun shippingToDtoTransformer(shipping: Shipping): ShippingDto {
+        return ShippingDto(
+                shipping.uuid.toString(),
+                shipping.city,
+                shipping.street,
+                shipping.streetNumber,
+                shipping.status
+        )
     }
 }
