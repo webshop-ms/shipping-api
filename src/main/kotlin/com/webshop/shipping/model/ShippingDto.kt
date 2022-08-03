9876adc5524a60a98fe2d22ca6961e3c93896671
Szabolcs Torma <szabolcs.torma@accretivetg.com>
2022-08-03 18:31:38 +0200

    shipping dto

---
+++ src/main/kotlin/com/webshop/shipping/model/ShippingDto.kt
@@ -0,0 +1,11 @@
+package com.webshop.shipping.model
+
+import com.webshop.shipping.entity.ShippingStatus
+
+class ShippingDto(
+        val id: String,
+        val city: String,
+        val street: String,
+        val streetNumber: Int,
+        val status: ShippingStatus,
+)
